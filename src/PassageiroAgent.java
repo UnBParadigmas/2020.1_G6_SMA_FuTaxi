845341ef1e96b46b91dda1cebd63d20a1184af26
jpmota2208 <jpmota.unb@gmail.com>
2020-11-16 23:05:04 -0300

    adiciona comportamento do passageiro

--- src/PassageiroAgent.java
+++ src/PassageiroAgent.java
@@ -5,8 +5,8 @@ import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAAgentManagement.DFAgentDeion;
+import jade.domain.FIPAAgentManagement.ServiceDeion;
 import jade.gui.DFAgentDscDlg;
 
 public class PassageiroAgent extends Agent {
@@ -33,12 +33,12 @@ public class PassageiroAgent extends Agent {
 				protected void onTick() {
 					System.out.println("Quero ir para "+localDeDestino);
 					// Update the list of seller agents
-					DFAgentDescription template = new DFAgentDescription();
-					ServiceDescription sd = new ServiceDescription();
+					DFAgentDeion template = new DFAgentDeion();
+					ServiceDeion sd = new ServiceDeion();
 					sd.setType("realizar-viagem");
 					template.addServices(sd);
 					try {
-						DFAgentDescription[] result = DFService.search(myAgent, template); 
+						DFAgentDeion[] result = DFService.search(myAgent, template); 
 						System.out.println("Motoristas encontrados:");
 						motoristasAgents = new AID[result.length];
 						for (int i = 0; i < result.length; ++i) {
@@ -68,4 +68,101 @@ public class PassageiroAgent extends Agent {
 		System.out.println("Passageiro "+getAID().getName()+" realizou viagem.");
 	}
 
+	/**
+	   Inner class RequestPerformer.
+	   This is the behaviour used by Book-buyer agents to request seller 
+	   agents the target book.
+	 */
+	private class RequestPerformer extends Behaviour {
+		private AID motoristaProximo; // The agent who provides the best offer 
+		private int menorPreco;  // The best offered price
+		private int repliesCnt = 0; // The counter of replies from seller agents
+		private MessageTemplate mt; // The template to receive replies
+		private int step = 0;
+
+		public void action() {
+			switch (step) {
+			case 0:
+				// Send the cfp to all sellers
+				ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+				for (int i = 0; i < motoristasAgents.length; ++i) {
+					cfp.addReceiver(motoristasAgents[i]);
+				} 
+				cfp.setContent(localDeDestino);
+				cfp.setConversationId("corrida");
+				cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
+				myAgent.send(cfp);
+				// Prepare the template to get proposals
+				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("corrida"),
+						MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+				step = 1;
+				break;
+			case 1:
+				// Receive all proposals/refusals from seller agents
+				ACLMessage reply = myAgent.receive(mt);
+				if (reply != null) {
+					// Reply received
+					if (reply.getPerformative() == ACLMessage.PROPOSE) {
+						// This is an offer 
+						int distancia = Integer.parseInt(reply.getContent());
+						if (motoristaProximo == null || distancia < menorPreco) {
+							// This is the best offer at present
+							menorPreco = distancia;
+							motoristaProximo = reply.getSender();
+						}
+					}
+					repliesCnt++;
+					if (repliesCnt >= motoristasAgents.length) {
+						// We received all replies
+						step = 2; 
+					}
+				}
+				else {
+					block();
+				}
+				break;
+			case 2:
+				// Send the purchase order to the seller that provided the best offer
+				ACLMessage order = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+				order.addReceiver(motoristaProximo);
+				order.setContent(localDeDestino);
+				order.setConversationId("corrida");
+				order.setReplyWith("order"+System.currentTimeMillis());
+				myAgent.send(order);
+				// Prepare the template to get the purchase order reply
+				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("corrida"),
+						MessageTemplate.MatchInReplyTo(order.getReplyWith()));
+				step = 3;
+				break;
+			case 3:      
+				// Receive the purchase order reply
+				reply = myAgent.receive(mt);
+				if (reply != null) {
+					// Purchase order reply received
+					if (reply.getPerformative() == ACLMessage.INFORM) {
+						// Purchase successful. We can terminate
+						System.out.println("Motorista " +reply.getSender().getName() +" encontrado para " + localDeDestino);
+						System.out.println("Preço = "+ menorPreco);
+						myAgent.doDelete();
+					}
+					else {
+						System.out.println("Nenhum táxi disponível");
+					}
+
+					step = 4;
+				}
+				else {
+					block();
+				}
+				break;
+			}        
+		}
+
+		public boolean done() {
+			if (step == 2 && motoristaProximo == null) {
+				System.out.println("Nenhum táxi disponível para esse local");
+			}
+			return ((step == 2 && motoristaProximo == null) || step == 4);
+		}
+	}  // End of inner class RequestPerformer
 }

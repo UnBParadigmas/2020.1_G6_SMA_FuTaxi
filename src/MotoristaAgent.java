909a3d0886706bbfbbaf38d5b40d63c86be3c246
Heron Rodrigues <heronrs1@gmail.com>
2020-11-16 22:43:10 -0300

    Adicona comportamentos do motorista

--- src/MotoristaAgent.java
+++ src/MotoristaAgent.java
@@ -31,6 +31,12 @@ public class MotoristaAgent extends Agent {
 		catch (FIPAException fe) {
 			fe.printStackTrace();
 		}
+
+		// Add the behaviour serving queries from buyer agents
+		addBehaviour(new OfferRequestsServer());
+
+		// Add the behaviour serving purchase orders from buyer agents
+		addBehaviour(new PurchaseOrdersServer());
 	}
 
 	// Put agent clean-up operations here
@@ -57,4 +63,59 @@ public class MotoristaAgent extends Agent {
 			}
 		} );
 	}
+
+	private class OfferRequestsServer extends CyclicBehaviour {
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) {
+				// CFP Message received. Process it
+				String local = msg.getContent();
+				ACLMessage reply = msg.createReply();
+
+				Integer preco = (Integer) localDeAtuacao.get(local);
+				if (preco != null) {
+					// The requested book is available for sale. Reply with the price
+					reply.setPerformative(ACLMessage.PROPOSE);
+					reply.setContent(String.valueOf(preco.intValue()));
+				}
+				else {
+					// The requested book is NOT available for sale.
+					reply.setPerformative(ACLMessage.REFUSE);
+					reply.setContent("not-available");
+				}
+				myAgent.send(reply);
+			}
+			else {
+				block();
+			}
+		}
+	}  // End of inner class OfferRequestsServer
+
+	private class PurchaseOrdersServer extends CyclicBehaviour {
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) {
+				// ACCEPT_PROPOSAL Message received. Process it
+				String local = msg.getContent();
+				ACLMessage reply = msg.createReply();
+
+				Integer preco = (Integer) localDeAtuacao.remove(local);
+				if (preco != null) {
+					reply.setPerformative(ACLMessage.INFORM);
+					System.out.println("Viagem para " +local+" realizada. Passageiro"+msg.getSender().getName());
+				}
+				else {
+					// The requested book has been sold to another buyer in the meanwhile .
+					reply.setPerformative(ACLMessage.FAILURE);
+					reply.setContent("not-available");
+				}
+				myAgent.send(reply);
+			}
+			else {
+				block();
+			}
+		}
+	}  // End of inner class OfferRequestsServer
 }
